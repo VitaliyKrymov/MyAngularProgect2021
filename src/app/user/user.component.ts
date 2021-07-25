74eebd4249288acc87eadafe9ab944be32124aba
VitaliyKrymov <krymvitaliy@gmail.com>
2021-07-25 20:24:45 +0300

    start angular lesson1

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component,Input, OnInit } from '@angular/core';
+import {IUserModel} from "../models/userModel";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+@Input ()
+  userObj: IUserModel;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
