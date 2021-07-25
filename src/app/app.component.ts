89957945c72917f310fb18e5a6ab5d9475799545
VitaliyKrymov <krymvitaliy@gmail.com>
2021-07-25 13:12:16 +0300

    start angular lesson1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template:'<h1>{{title}}</h1> <br> {{title}}',
+  // styles:['body{background: silver}']
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'MyAngularProgect2021';
+  users =[
+    {name:'vasia',age:'23',status:false},
+    {name:'tania',age:'63',status:true},
+    {name:'vania',age:'33',status:false},
+    {name:'valia',age:'13',status:true},
+    {name:'vadim',age:'43',status:false},
+  ];
 }
